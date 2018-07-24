ed5aa5c0ddcbc605a23a4d47a08792ea03b3f0b2
Niels Ott <niels.ott@integrata-stiftung.de>
2018-07-24 16:10:49 +0200

    ShapePermission now says hello when its methods are being called.

--- docroot/WEB-INF/src/org/politaktiv/map/service/permission/ShapePermission.java
+++ docroot/WEB-INF/src/org/politaktiv/map/service/permission/ShapePermission.java
@@ -2,21 +2,30 @@
 
 package org.politaktiv.map.service.permission;
 
+import org.apache.log4j.Logger;
+import org.politaktiv.map.model.Shape;
+
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.security.auth.PrincipalException;
 import com.liferay.portal.security.permission.PermissionChecker;
-import org.politaktiv.map.model.Shape;
 
 public class ShapePermission {
 
     public static final String ADD_AND_UPDATE_PERSONAL_SHAPE = "ADD_AND_UPDATE_PERSONAL_SHAPE";
     public static final String UPDATE_ANY_SHAPE_ACTION = "UPDATE_ANY_SHAPE";
+    
+    public static void sayHelloToDebug()    {
+    	Logger LOGGER = Logger.getLogger(ShapePermission.class);
+      final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+      LOGGER.info("Hello from " + ste[2].getMethodName() + "(...)");
+    }
 
 
     private ShapePermission() {
 	}
 
 	public static void checkUpdate(PermissionChecker permissionChecker, long groupId, String resourceName, String primKey, long userId) throws PortalException {
+		sayHelloToDebug();
 		if (canAddAndUpdatePersonalShape(permissionChecker, groupId, resourceName, primKey)) {
 			return;
 		}
@@ -27,6 +36,7 @@ public class ShapePermission {
 	}
 	
 	public static void checkAddAndUpdatePersonalShape(PermissionChecker permissionChecker, long groupId, String resourceName, String primKey) throws PortalException {
+		sayHelloToDebug() ;
 		if (!canAddAndUpdatePersonalShape(permissionChecker, groupId, resourceName, primKey)) {
 			throw new PrincipalException("User " + permissionChecker.getUserId() + " has no permission to make action " + ADD_AND_UPDATE_PERSONAL_SHAPE);
 		}
@@ -34,15 +44,17 @@ public class ShapePermission {
 
 
 	public static boolean canAddAndUpdatePersonalShape(PermissionChecker permissionChecker, long groupId, String resourceName, String primKey) {
+		sayHelloToDebug();
 		return contains(permissionChecker, groupId, resourceName, primKey, ADD_AND_UPDATE_PERSONAL_SHAPE);
 	}
 	
 	public static boolean canUpdateAnyShapes(PermissionChecker permissionChecker, long groupId, String resourceName, String primKey) {
+		sayHelloToDebug() ;
 		return contains(permissionChecker, groupId, resourceName, primKey, UPDATE_ANY_SHAPE_ACTION);
 	}
 	
     public static boolean canUpdateShape(PermissionChecker permissionChecker, Shape shape, String resourceName, String primKey) {
-
+    	sayHelloToDebug() ;
        return (((permissionChecker.getUserId()==shape.getUserId())&&canAddAndUpdatePersonalShape(permissionChecker,shape.getGroupId(),resourceName,primKey))||
                canUpdateAnyShapes(permissionChecker,shape.getGroupId(),resourceName,primKey));
 
@@ -50,6 +62,7 @@ public class ShapePermission {
 
 	public static boolean contains(PermissionChecker permissionChecker, long groupId, String resourceName,
 			String primKey, String actionId) {
+		sayHelloToDebug() ;
 		return permissionChecker.hasPermission(groupId, resourceName, primKey, actionId);
 	}
 
